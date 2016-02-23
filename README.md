650cb9629ad49c89c972d7cc72569a1dcf62fbba
Alan Shaw <alan@nodename.com>
2016-02-23 01:40:47 -0500

    Updated README

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# StateChart
+# Stately: StateCharts in the re-frame Environment
 
 ## Components
 
@@ -15,17 +15,12 @@ Arguments to the function may be provided by enclosing the function and argument
 
 ## Transitions
 
-A transition must specify start-states; it will not fire if the current state
-is not one of the start-states. Start-states may be specified as `:any` or `:none` if desired.
+A transition must specify a start-state. Its key in the handler map is a vector of start-state and trigger.
 
-The transition must also specify a target state. This may be identical to a start state.
-Or it may be specified as `:internal`, indicating no change of state.
+The transition must also specify a target state. This may be identical to its start state,
+or it may be specified as `:internal`, indicating no change of state.
 (An :internal transition will not invoke exit or entry actions of the state.)
 The target may also be a function that will yield the target state when the transition is invoked.
-(There are rare transitions whose target state cannot be determined until after the transition,
-such as popping the screen stack when a back button is pressed; in this case you must
-ensure that `complete-transition` is invoked.)
-
 A transition may also have actions; these are triggered when the transition takes place,
 and are invoked in the context of the target state.
 (These actions may in turn trigger other transitions.) If omitted, actions
@@ -34,27 +29,16 @@ defaults to `[]`.
 A transition may have a condition: a function that will prevent the transition
 from triggering if it returns a falsy value in the handler.
 A condition has the same signature as an event handler, but it does not return the new db.
-(Condition defaults to `(constantly true)`.)
+(Conditions default to `(constantly true)`.)
 
-Here's an example. Suppose we want to accept no passwords except 'boo'.
-We give the transition a vector of branches, each having its own condition, target,
-and actions.
-
+Here's an example from QuantumCalc. We have already accepted an operator, but we want to handle a negative second operand. From the :cal/op-entered state, we will accept another :operator-entered trigger and transition into the :operand2/int state, but only if the second operator is a minus:
 
 ```
-:signin/go-chosen {:start-states [:signin/start]
-                   :branches [{:condition (fn [db _]
-                                            (= (get-in db [:values :password]) "boo"))
-                               :target       :signin/in-progress
-                               :actions      [[:signin/start-action]]}
-
-                              {:target       :signin/forbidden}]}
+[:calc/op-entered :operator-entered] {:condition (fn [db [value]]
+                                                   (= value '-))
+                                      :target :operand2/int}
 ```
 
-The first branch whose condition evaluates to `true` will be executed.
-
-Any field of a transition that appears outside of the `:branches` is regarded as
-common to all the branches.
 
 ## States
 
@@ -82,10 +66,6 @@ start-states that will then be entered recursively.
 If a component specifies a start-state, when the component is entered,
 so will the start-state be entered.
 
-
-All keyword identifiers for actions, transitions, and states must be namespaced.
-We use the namespaces for the conceptual decomposition of our statechart
-into the various components. We also rely upon namespaces to implement event bubbling.
 
 Application code should not directly invoke action functions. It should only dispatch events.
 Actions are invoked by transitions in the same statechart component.
--- examples/QuantumCalc/src/quantumcalc/statechart/calc-bak.cljs
+++ examples/QuantumCalc/src/quantumcalc/statechart/calc-bak.cljs
@@ -16,9 +16,12 @@
                                                    (dispatch [:calc/clear-value-action])
                                                    db)}
 
-          :transitions {[:app/on :app/cancel-entered] {:target :app/on
-                                                       :actions [[:app/clear-inputs-action]
-                                                                 [:calc/clear-result-action]]}}
+          :transitions {[:app/on :cancel-entered] {:target :app/on
+                                                   :actions [[:app/clear-inputs-action]
+                                                             [:calc/clear-result-action]]}
+                        [:app/on :zero-entered] {:target :operand1/zero}
+                        [:app/on :digit-entered] {:target :operand1/int}
+                        [:app/on :dot-entered] {:target :operand1/frac}}
 
           :start-state :app/on
 
@@ -26,49 +29,40 @@
 
 
 (defonce calc
-         {:actions {:calc/update-value-action            [append-value :calc/value]
-                    :calc/replace-operator-action        replace-operator
+         {:actions {:calc/replace-operator-action        replace-operator
                     :calc/clear-value-action             [clear-value :calc/value]
                     :calc/calculate-result-action        calculate-result
                     :calc/clear-result-action            clear-result
                     :calc/copy-result-to-operand1-action copy-result-to-operand1}
 
-          :transitions {[:calc/start :calc/zero-entered] {:target :operand1/zero}
-                        [:calc/start :calc/digit-entered] {:target :operand1/int}
-                        [:calc/start :calc/dot-entered] {:target :operand1/frac}
+          :transitions {[:calc/operand1 :operator-entered] {:target :calc/op-entered
+                                                            :actions [[:calc/replace-operator-action]]}
 
 
-                        [:calc/operand1 :calc/operator-entered] {:target :calc/op-entered
-                                                                 :actions [[:calc/replace-operator-action]]}
+                        [:calc/op-entered :zero-entered] {:target :operand2/zero}
+                        [:calc/op-entered :digit-entered] {:target :operand2/int}
+                        [:calc/op-entered :dot-entered] {:target :operand2/frac}
 
-
-                        [:calc/op-entered :calc/zero-entered] {:target :operand2/zero}
-                        [:calc/op-entered :calc/digit-entered] {:target :operand2/int}
-                        [:calc/op-entered :calc/dot-entered] {:target :operand2/frac}
-
-                        ;; insert neg2.txt here to enable a negative second operand
+                        ;; insert neg2.txt to enable a negative second operand
 
 
 
-                        [:calc/operand2 :calc/equals-entered] {:target  :calc/result
-                                                               :actions [[:calc/calculate-result-action]
-                                                                         [:app/clear-inputs-action]]}
+                        [:calc/operand2 :equals-entered] {:target  :calc/result
+                                                          :actions [[:calc/calculate-result-action]
+                                                                    [:app/clear-inputs-action]]}
 
-                        ;; insert xx.txt here to enable chaining like operand operator operand operator operand...
+                        ;; insert xx.txt to enable chaining, like operand operator operand operator operand...
 
 
 
-                        [:calc/result :calc/zero-entered] {:target :operand1/zero}
-                        [:calc/result :calc/digit-entered] {:target :operand1/int}
-                        [:calc/result :calc/dot-entered] {:target :operand1/frac}
-                        [:calc/result :calc/operator-entered] {:target :calc/op-entered
-                                                               :actions   [[:calc/copy-result-to-operand1-action]
-                                                                           [:calc/replace-operator-action]]}}
+                        [:calc/result :zero-entered] {:target :operand1/zero}
+                        [:calc/result :digit-entered] {:target :operand1/int}
+                        [:calc/result :dot-entered] {:target :operand1/frac}
+                        [:calc/result :operator-entered] {:target :calc/op-entered
+                                                          :actions   [[:calc/copy-result-to-operand1-action]
+                                                                      [:calc/replace-operator-action]]}}
 
-          :start-state :calc/start
-
-          :states {:calc/start      {}
-                   :calc/operand1   {:components [:operand1]}
+          :states {:calc/operand1   {:components [:operand1]}
                    :calc/op-entered {}
                    :calc/operand2   {:components [:operand2]}
                    :calc/result     {}}})
@@ -80,20 +74,20 @@
            :operandX/clear-value-action  [clear-value :operandX/value]}
 
           :transitions
-          {[:operandX/zero :operandX/zero-entered] {:target :internal}
-           [:operandX/zero :operandX/dot-entered] {:target :operandX/frac}
-           [:operandX/zero :operandX/digit-entered] {:target :operandX/int}
+          {[:operandX/zero :zero-entered] {:target :internal}
+           [:operandX/zero :dot-entered] {:target :operandX/frac}
+           [:operandX/zero :digit-entered] {:target :operandX/int}
 
-           [:operandX/int :operandX/zero-entered] {:target :internal
-                                                   :actions [[:operandX/update-value-action]]}
-           [:operandX/int :operandX/dot-entered] {:target :operandX/frac}
-           [:operandX/int :operandX/digit-entered] {:target :internal
-                                                    :actions [[:operandX/update-value-action]]}
+           [:operandX/int :zero-entered] {:target :internal
+                                          :actions [[:operandX/update-value-action]]}
+           [:operandX/int :dot-entered] {:target :operandX/frac}
+           [:operandX/int :digit-entered] {:target :internal
+                                           :actions [[:operandX/update-value-action]]}
 
-           [:operandX/frac :operandX/zero-entered] {:target :internal
-                                                    :actions [[:operandX/update-value-action]]}
-           [:operandX/frac :operandX/digit-entered] {:target :internal
-                                                     :actions [[:operandX/update-value-action]]}}
+           [:operandX/frac :zero-entered] {:target :internal
+                                           :actions [[:operandX/update-value-action]]}
+           [:operandX/frac :digit-entered] {:target :internal
+                                            :actions [[:operandX/update-value-action]]}}
 
           :states
           {:operandX/zero  {:entry-actions [[:operandX/update-value-action]]}
